9747bba57b3adabb5656c5cf100f50fbf0dcc1ff
SaJesh Shrestha <sajeshshrestha@SaJeshs-MacBook-Air.local>
2021-02-05 10:40:04 +0545

    Implement Network Connectivity

--- MovieList/MovieList/ViewController.swift
+++ MovieList/MovieList/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import Network
 
 class ViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
     var movies: [MovieProp] = []
     var images: [UIImage] = []
     var coreDataMovie: [MovieProps] = []
+    var isNetwork = false
     
     
     override func viewDidLoad() {
@@ -29,7 +31,25 @@ class ViewController: UIViewController {
         configureTableView()
         tableUILayout()
 //        fetchData(endpoint: endpoint)
-        fetchFromCoreData()
+//        fetchFromCoreData()
+        monitorNetwork()
+    }
+    
+    func monitorNetwork() {
+        let monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                DispatchQueue.main.async {
+                    self.fetchData(endpoint: self.endpoint)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.fetchFromCoreData()
+                }
+            }
+        }
+        let queue = DispatchQueue(label: "Network")
+        monitor.start(queue: queue)
     }
     
     
@@ -41,7 +61,6 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.identifer)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        
     }
     
     
@@ -57,7 +76,9 @@ class ViewController: UIViewController {
     
     
     func fetchData(endpoint: String) {
+        isNetwork = true
         showLoadingView()
+        print("JSON Data")
         if let url = URL(string: endpoint) {
             URLSession.shared.dataTask(with: url) { (data, response, error) in
                 if error != nil {
@@ -92,8 +113,23 @@ class ViewController: UIViewController {
     }
     
     
+    func fetchFromCoreData() {
+        isNetwork = false
+        print("CoreData")
+        do {
+            coreDataMovie = try context.fetch(MovieProps.fetchRequest())
+        } catch {
+            print("Error while fetching... \(error)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    
     func downloadImage(from string: String) {
-//        self.images.removeAll()
+        //        self.images.removeAll()
         guard let url = URL(string: string)
         else {
             print("Unable to create URL")
@@ -122,19 +158,6 @@ class ViewController: UIViewController {
         }
         return img
     }
-    
-    
-    func fetchFromCoreData() {
-        do {
-            coreDataMovie = try context.fetch(MovieProps.fetchRequest())
-        } catch {
-            print("Error while fetching... \(error)")
-        }
-        
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-        }
-    }
 }
 
 
@@ -147,24 +170,29 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifer, for: indexPath) as! MovieTableViewCell
         
-//        if !movies.isEmpty {
-//
-//            cell.imdbID.text = movies[indexPath.row].imdbID
-//            cell.titleLabel.text = movies[indexPath.row].movieTitle
-//            cell.year.text = movies[indexPath.row].year
-//            cell.posterImageView.image = images[indexPath.row]
-//        } else {
-//            cell.imdbID.text = "yo boie"
-//            cell.titleLabel.text = "yo boie"
-//            cell.year.text = "yo boie"
-//            cell.posterImageView.image = #imageLiteral(resourceName: "noimage")
-//        }
+        //TODO: Display json fetched data
         
-        if !coreDataMovie.isEmpty {
-            cell.imdbID.text = coreDataMovie[indexPath.row].imdbID
-            cell.titleLabel.text = coreDataMovie[indexPath.row].title
-            cell.year.text = coreDataMovie[indexPath.row].year
-            cell.posterImageView.image = UIImage(data: coreDataMovie[indexPath.row].posterImage!)
+        if isNetwork {
+            print("Inside network")
+            if !movies.isEmpty {
+                cell.imdbID.text = movies[indexPath.row].imdbID
+                cell.titleLabel.text = movies[indexPath.row].movieTitle
+                cell.year.text = movies[indexPath.row].year
+                cell.posterImageView.image = images[indexPath.row]
+            } else {
+                cell.imdbID.text = "yo boie"
+                cell.titleLabel.text = "yo boie"
+                cell.year.text = "yo boie"
+                cell.posterImageView.image = #imageLiteral(resourceName: "noimage")
+            }
+        } else {
+            print("Inside CoreData")
+            if !coreDataMovie.isEmpty {
+                cell.imdbID.text = coreDataMovie[indexPath.row].imdbID
+                cell.titleLabel.text = coreDataMovie[indexPath.row].title
+                cell.year.text = coreDataMovie[indexPath.row].year
+                cell.posterImageView.image = UIImage(data: coreDataMovie[indexPath.row].posterImage!)
+            }
         }
         
         return cell
